f0bac9de86a175d34ac8d693410ba6d895cd864d
lucas-morais <lucasmoraisss@gmail.com>
2020-02-01 16:38:50 -0300

    BD2

--- src/data/create-mysql-tables.py
+++ src/data/create-mysql-tables.py
@@ -1,20 +1,23 @@
 import pandas as pd
-
-
 import sqlalchemy
 import os
 from dotenv import load_dotenv, find_dotenv
 from sqlalchemy import create_engine
 
-dotenv_path = find_dotenv()
-print(dotenv_path)
 
+
+## Acessando Banco de Dados
+
+#Variáveis de Ambiente
+dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
-database_con_string = os.environ.get("DATBASE_USER")
-print(database_con_string)
-#engine = create_engine(database_con_string, echo=True)
-#print("Usuário:", database_user)
-#print("Senha:", database_password)
+
+#String de conexão com MySQL
+database_con_string = os.environ.get("DATABASE_CON")
+
+#Conexão 
+engine = create_engine(database_con_string, echo=True)
+
 
 
 
